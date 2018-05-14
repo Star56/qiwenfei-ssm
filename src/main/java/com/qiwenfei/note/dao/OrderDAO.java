2cbe2cdccba0e884939c39396d25a8364ea6de23
sun_shine <38171404@qq.com>
2018-05-14 23:50:49 +0800

    learning-01

---
+++ src/main/java/com/qiwenfei/note/dao/OrderDAO.java
@@ -0,0 +1,19 @@
+package com.qiwenfei.note.dao;
+
+import com.qiwenfei.note.entity.OrderInfo;
+
+/**
+ * 2018-05-14
+ * sunshine
+ */
+public class OrderDAO {
+
+    public static void main(String[] args) {
+        OrderInfo orderInfo = new OrderInfo();
+        orderInfo.setId(1L);
+        orderInfo.setName("笔记本");
+
+        System.out.println(orderInfo);
+
+    }
+}
---
+++ src/main/java/com/qiwenfei/note/entity/OrderInfo.java
@@ -0,0 +1,36 @@
+package com.qiwenfei.note.entity;
+
+/**
+ * 2018-05-14
+ * sunshine
+ */
+public class OrderInfo  {
+
+    private Long id ;
+
+    private String name ;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderInfo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
