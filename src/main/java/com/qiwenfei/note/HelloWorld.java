f49b2a21c9276c741b08acbb338d0184dac1c682
sun_shine <38171404@qq.com>
2018-05-14 22:25:14 +0800

    Initial commit

---
+++ src/main/java/com/qiwenfei/note/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.qiwenfei.note;
+
+/**
+ * 2018-05-14
+ * sunshine
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
