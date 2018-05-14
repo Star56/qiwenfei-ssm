c6817a54e31314298c1661db487b63ca85341784
sun_shine <38171404@qq.com>
2018-05-14 22:40:43 +0800

    test

--- src/main/java/com/qiwenfei/note/HelloWorld.java
+++ src/main/java/com/qiwenfei/note/HelloWorld.java
@@ -8,9 +8,4 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 }
